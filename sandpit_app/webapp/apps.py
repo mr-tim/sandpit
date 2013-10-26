fea2730513096cbf42f234f33e77b31c9cc21ed4
Tim Moran <tim@t-moran.com>
2013-10-26 22:25:53 +0100

    Switch to using db rather than raw sqlalchemy sessions

--- sandpit_app/webapp/apps.py
+++ sandpit_app/webapp/apps.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from flask import Blueprint, redirect, render_template, request
 import re
 
-from models import App, AppImage, Session
+from core import current_user, db
+from models import App, AppImage
 import security
 import tasks
 
@@ -15,8 +16,7 @@ apps = Blueprint('apps', __name__)
 @apps.route('/')
 @security.logged_in
 def list_apps():
-    session = Session()
-    apps = session.query(App).order_by('name').all()
+    apps = current_user.apps
     return render_template('apps.html', current_tab='apps', apps=apps)
 
 @apps.route('/app', methods=['POST'])
@@ -24,23 +24,20 @@ def list_apps():
 def create_app():
     app_id = slugify(request.form['newAppName'])
     app = App(id=app_id, name=request.form['newAppName'], app_type=request.form['appType'])
-    session = Session()
-    session.add(app)
-    session.commit()
+    db.add(app)
+    db.commit()
     return redirect('/')
 
 @apps.route('/app/<app_id>')
 @security.logged_in
 def app_details(app_id):
-    session = Session()
-    app = session.query(App).get(app_id)
+    app = db.query(App).get(app_id)
     return render_template('app.html', app=app, current_tab='apps')
 
 @apps.route('/app/<app_id>/createImage', methods=['GET'])
 @security.logged_in
 def create_image_form(app_id):
-    session = Session()
-    app = session.query(App).get(app_id)
+    app = db.query(App).get(app_id)
 
     if app.app_type == 'python':
         template = 'createPythonImage.html'
@@ -52,14 +49,13 @@ def create_image_form(app_id):
 @apps.route('/app/<app_id>/createImage', methods=['POST'])
 @security.logged_in
 def create_image(app_id):
-    session = Session()
-    app = session.query(App).get(app_id)
+    app = db.query(App).get(app_id)
 
     if app.app_type == 'python':
         url = request.form['packageUrl']
         app_image = AppImage(name=request.form['name'], status='Pending build since %s' % datetime.now().strftime('%Y-%m-%d %H:%M:%S'), app=app)
-        session.add(app_image)
-        session.commit()
+        db.add(app_image)
+        db.commit()
         tasks.build_image.delay(app_image.id, 'shykes/pybuilder:latest', ['/usr/local/bin/buildapp', url])
     else:
         pass
