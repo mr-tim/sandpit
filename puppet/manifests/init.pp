49155d4b20ed48e0ab64a8a5c76974bde8c48a03
Tim Moran <tim@t-moran.com>
2013-11-16 10:36:13 +0000

    Move puppet configuration into a separate type within the sandpit module

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -2,25 +2,8 @@ package { ["nginx", "curl", "htop", "nmap", "rabbitmq-server"]:
   ensure => "installed"
 }
 
-file { "/etc/nginx/sites-available/sandpit":
-  source => "puppet:///modules/sandpit/nginx/sandpit.conf",
-  ensure => "present"
-}
-
-file { "/etc/nginx/sites-enabled/sandpit":
-  ensure => "link",
-  target => "/etc/nginx/sites-available/sandpit",
-  require => [File["/etc/nginx/sites-available/sandpit"], Package['nginx']]
-}
-
-file { "/etc/init/sandpit-app.conf":
-  source => "puppet:///modules/sandpit/sandpit-app.conf",
-  ensure => "present"
-}
-
-file { "/etc/init/sandpit-celery.conf":
-  source => "puppet:///modules/sandpit/sandpit-celery.conf",
-  ensure => "present"
+sandpit::server { 'sandpit-install':
+  require => [Package['nginx'], Service['rabbitmq-server']]
 }
 
 service { "nginx":
@@ -28,24 +11,8 @@ service { "nginx":
   ensure => "running"
 }
 
-service { "sandpit-app": 
-  require => File["/etc/init/sandpit-app.conf"],
-  ensure => "running"
-}
-
 service { "rabbitmq-server":
   ensure => "running"
-}
-
-service { "sandpit-celery": 
-  require => [File["/etc/init/sandpit-celery.conf"], Service['rabbitmq-server']],
-  ensure => "running"
-}
-
-file { ["/sandpit", "/sandpit/sessions", "/sandpit/sessions/data", "/sandpit/sessions/lock"]:
-  ensure => "directory",
-  owner => "vagrant",
-  group => "vagrant"
 }
 
 
---
+++ puppet/modules/sandpit/manifests/server.pp
@@ -0,0 +1,48 @@
+define sandpit::server() {
+
+  file { "/etc/nginx/sites-available/sandpit":
+    source => "puppet:///modules/sandpit/nginx/sandpit.conf",
+    ensure => "present"
+  }
+
+  file { "/etc/nginx/sites-enabled/sandpit":
+    ensure => "link",
+    target => "/etc/nginx/sites-available/sandpit",
+    require => File["/etc/nginx/sites-available/sandpit"]
+  }
+
+  file { "/etc/init/sandpit-app.conf":
+    source => "puppet:///modules/sandpit/sandpit-app.conf",
+    ensure => "present"
+  }
+
+  file { "/etc/init/sandpit-celery.conf":
+    source => "puppet:///modules/sandpit/sandpit-celery.conf",
+    ensure => "present"
+  }  
+
+  service { "sandpit-app": 
+    require => File["/etc/init/sandpit-app.conf"],
+    ensure => "running"
+  }
+
+  service { "sandpit-celery": 
+    require => File["/etc/init/sandpit-celery.conf"],
+    ensure => "running"
+  }
+
+  file { ["/sandpit", "/sandpit/sessions", "/sandpit/sessions/data", "/sandpit/sessions/lock"]:
+    ensure => "directory",
+    owner => "vagrant",
+    group => "vagrant"
+  }
+
+  file { "/etc/nginx/nginx.conf":
+    source => "puppet:///modules/sandpit/nginx/nginx.conf",
+    ensure => "present",
+    owner => 'root',
+    group => 'root',
+    notify => Service["nginx"]
+  }
+
+}
